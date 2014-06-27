f176d192baf547a76fc08576a82f9f4ff3b70464
Sjava <anisimow@ua.fm>
2014-06-27 10:54:36 +0300

    ext_action_required добавил
    Добавил обработку исключения ext_action_required. Теперь при таком исключении перенаправляет по адрессу в переменной ext_action_uri

--- yamoney/controllers/front/redirect.php
+++ yamoney/controllers/front/redirect.php
@@ -84,6 +84,9 @@ class yamoneyredirectModuleFrontController extends ModuleFrontController
                 $this->module->card_allowed = (!empty($MoneySource['card']['allowed'])) ? $MoneySource['card']['allowed'] : false;
                 $this->module->wallet_alowed = (!empty($MoneySource['wallet']['allowed'])) ? $MoneySource['wallet']['allowed'] : false;
              } else {
+             	 if($resp->getError() == 'ext_action_required' && $resp->getStatus() == 'refused'){
+                     Tools::redirect($resp->getExtActionUri());
+                 }
                  $this->errors[]=$this->module->descriptionError($resp->getError());
                  return;
              }        
